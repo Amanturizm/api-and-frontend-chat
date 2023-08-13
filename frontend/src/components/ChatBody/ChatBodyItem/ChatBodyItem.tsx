fa0606825785cad3b4b8fe0cf51567da82a23a43
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-08-13 15:33:09 +0600

    added dayjs

--- frontend/src/components/ChatBody/ChatBodyItem/ChatBodyItem.tsx
+++ frontend/src/components/ChatBody/ChatBodyItem/ChatBodyItem.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Avatar, Box, Card, Stack, Typography } from "@mui/material";
+import dayjs from "dayjs";
 
 interface Props {
 	message: IMessage;
@@ -8,10 +9,8 @@ interface Props {
 const colors = ["#FF0000", "#0000FF", "#00FF00", "#FFFF00", "#FFC0CB", "#800080", "#FFA500"];
 
 const ChatBodyItem: React.FC<Props> = ({ message }) => {
-	const dateObj = new Date(message.datetime!);
-	const now = new Date();
-
-	const timeDifference = now.getTime() - dateObj.getTime();
+	const date = dayjs(message.datetime).format('DD/MM/YYYY HH:mm:ss');
+	const timeDifference = -dayjs(message.datetime).diff();
 
 	const daysPassed: number = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
 	const hoursPassed: number = Math.floor(timeDifference / (1000 * 60 * 60));
@@ -19,7 +18,8 @@ const ChatBodyItem: React.FC<Props> = ({ message }) => {
 	const secondsPassed: number = Math.floor(timeDifference / 1000);
 
 	const timeResult: string =
-		daysPassed > 0 && daysPassed < 30 ? `${daysPassed} дней назад` :
+		daysPassed > 1 ? date :
+		daysPassed === 1 ? 'Вчера' :
 		hoursPassed > 0 && hoursPassed < 24 ? `${hoursPassed} часов назад` :
 		minutesPassed > 0 && minutesPassed < 60 ? `${minutesPassed} минут назад` :
 		secondsPassed < 4 ? 'только что' : `${secondsPassed} секунд назад`;
